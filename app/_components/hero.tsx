f3d892c615308c3418d0f72253ae9efee25b5f19
Fatema-Tuj-Zahura Moumita <fatihamoumita98@gmail.com>
2025-11-22 00:33:07 +0600

    banner carousel now showing

--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -1,53 +1,76 @@
-  "use client" 
- import React from "react";
+"use client";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/css/pagination';
-import "swiper/css";     
-import { Pagination } from "swiper/modules";
+import "swiper/css/pagination";
+import "swiper/css";
 
 export default function Hero() {
   return (
     <div className="p-4">
       <div className="h-[70vh] w-full relative overflow-hidden rounded-xl ">
-      <img
-        src="/assets/banner.jpg"
-        className="h-full w-full object-cover "
-        alt="banner"
-      />
-      <div className="absolute  w-full h-full inset-0 rounded-xl  bg-[rgba(0,0,0,0.4)] z-1 "></div>
-      <div className="banner-des absolute inset-0 z-10 text-white flex flex-col gap-4 items-center justify-center ">
-        <h1 className="text-7xl font-semibold">Find your next favorite thing.</h1>
-        <p className="text-2xl font-semibold">Discover and buy amazing things</p>
-       <div className="mt-12 border-4 border-white p-1 rounded-4xl">
-         <button className="btn px-4 py-2 bg-white text-black font-semibold rounded-4xl ">SHOP NOW</button>
-       </div>
+        <img
+          src="/assets/banner.jpg"
+          className="h-full w-full object-cover "
+          alt="banner"
+        />
+        <div className="absolute  w-full h-full inset-0 rounded-xl  bg-[rgba(0,0,0,0.4)] z-1 "></div>
+        <div className="banner-des absolute inset-0 z-10 text-white flex flex-col gap-4 items-center justify-center ">
+          <h1 className="text-7xl font-semibold">
+            Find your next favorite thing.
+          </h1>
+          <p className="text-2xl font-semibold">
+            Discover and buy amazing things
+          </p>
+          <div className="mt-12 border-4 border-white p-1 rounded-4xl">
+            <button className="btn px-4 py-2 bg-white text-black font-semibold rounded-4xl ">
+              SHOP NOW
+            </button>
+          </div>
+        </div>
+
+        {/* banner Carousel  */}
+        <div className="absolute bottom-5 left-0 w-full z-10 ">
+          <Swiper
+            slidesPerView={6}
+            centeredSlides={true}
+            spaceBetween={20}
+            grabCursor={true}
+            pagination={{
+              clickable: true,
+            }}
+            className="mySwiper h-[72px]"
+          >
+            <SwiperSlide className="bg-[rgb(255,254,254,0.3)] p-4 rounded-lg text-center max-w-[250px] text-white font-lg font-semibold hover:bg-[rgb(255,254,254,0.8)] hover:text-black ">
+              Perfume
+            </SwiperSlide>
+            <SwiperSlide className="bg-[rgb(255,254,254,0.3)] p-4 rounded-lg text-center max-w-[250px] text-white font-lg font-semibold hover:bg-[rgb(255,254,254,0.8)] hover:text-black ">
+              Kitchen
+            </SwiperSlide>
+            <SwiperSlide className="bg-[rgb(255,254,254,0.3)] p-4 rounded-lg text-center max-w-[250px] text-white font-lg font-semibold hover:bg-[rgb(255,254,254,0.8)] hover:text-black ">
+              Makeup
+            </SwiperSlide>
+            <SwiperSlide className="bg-[rgb(255,254,254,0.3)] p-4 rounded-lg text-center max-w-[250px] text-white font-lg font-semibold hover:bg-[rgb(255,254,254,0.8)] hover:text-black ">
+              Lipstick
+            </SwiperSlide>
+            <SwiperSlide className="bg-[rgb(255,254,254,0.3)] p-4 rounded-lg text-center max-w-[250px] text-white font-lg font-semibold hover:bg-[rgb(255,254,254,0.8)] hover:text-black ">
+              Women Clothes
+            </SwiperSlide>
+            <SwiperSlide className="bg-[rgb(255,254,254,0.3)] p-4 rounded-lg text-center max-w-[250px] text-white font-lg font-semibold hover:bg-[rgb(255,254,254,0.8)] hover:text-black ">
+              Mens Clothes
+            </SwiperSlide>
+            <SwiperSlide className="bg-[rgb(255,254,254,0.3)] p-4 rounded-lg text-center max-w-[250px] text-white font-lg font-semibold hover:bg-[rgb(255,254,254,0.8)] hover:text-black ">
+              Groceries
+            </SwiperSlide>
+            <SwiperSlide className="bg-[rgb(255,254,254,0.3)] p-4 rounded-lg text-center max-w-[250px] text-white font-lg font-semibold hover:bg-[rgb(255,254,254,0.8)] hover:text-black">
+              skin Care
+            </SwiperSlide>
+            <SwiperSlide className="bg-[rgb(255,254,254,0.3)] p-4 rounded-lg text-center max-w-[250px] text-white font-lg font-semibold hover:bg-[rgb(255,254,254,0.8)] hover:text-black">
+              {" "}
+              Baby product
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
-       
-       {/* banner Carousel  */}
-      <div className="relative z-10 h-[72px] ">
-        <Swiper
-        slidesPerView={4}
-        centeredSlides={true}
-        spaceBetween={30}
-        grabCursor={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-      </div>
-    </div>
     </div>
   );
 }
